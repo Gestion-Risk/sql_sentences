c6349f6d67f01f8e07346891ab6eecfae99ad066
cdospinav16 <cdospinav16@gmail.com>
2022-02-10 13:41:59 -0500

    Queries files

---
+++ sql_area.sql
@@ -0,0 +1,14 @@
+INSERT INTO public."AuthAppEmpleados_areacapacitacion"
+(area, descripcion)
+VALUES('Prevención de riesgos en oficina', 'Métodos y medidas de prevención'),
+('Prevención de riesgos en obra', 'Métodos y medidas de prevención'),
+('Dirección y sistemas de Gestión de SST y Auditorias', ' métodos del Liderazgo Preventivo'),
+('Salud ocupacional en obras', 'Salud y la calidad de vida de las personas en la ocupación laboral'),
+('Salud ocupacional en oficinas', 'Salud y la calidad de vida de las personas en la ocupación laboral'),
+('Comité de seguridad y salud ocupacional', 'Manejo del organismo de promoción y vigilancia de las normas y reglamentos de seguridad y salud en el trabajo'),
+('Prevención en altura', 'Métodos y medidas de prevención'),
+('Prevención en maquinaria', 'Métodos y medidas de prevención'),
+('Prevención en materiales', 'Métodos y medidas de prevención');
+
+
+
